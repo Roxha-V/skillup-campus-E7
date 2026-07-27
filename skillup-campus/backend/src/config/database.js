af5f7783f3aa7e783cd078742f00a8407fb8b34f
Fabricio <fabricionicolasr@gmail.com>
2026-07-27 00:22:51 -0300

    se agrega conexion a base de datos

---
+++ skillup-campus/backend/src/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+// Sequelize detecta automáticamente todos los datos a partir del string URL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Evita errores de certificados SSL en entornos de desarrollo
+    }
+  }
+});
