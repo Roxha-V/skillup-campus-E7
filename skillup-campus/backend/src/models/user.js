009f107583e938a117711ad0a8c43f018351776b
Fabricio <fabricionicolasr@gmail.com>
2026-07-28 12:57:14 -0300

    se modifica user para sequelize

--- skillup-campus/backend/src/models/user.js
+++ skillup-campus/backend/src/models/user.js
@@ -1,8 +1,32 @@
-class User {
-  constructor(name, email, password, role) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.role = role;
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isIn: {
+        args: [['student', 'admin']],
+        msg: 'El role debe ser student o admin'
+      }
+    }
   }
-}
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
