c319b14743630d9a727e55b71980c3b18f2acdb2
Fabricio <fabricionicolasr@gmail.com>
2026-08-04 01:21:57 -0300

    feature: se valida usuario duplicado. Se refactoriza el insert de role

--- skillup-campus/backend/src/controllers/authController.js
+++ skillup-campus/backend/src/controllers/authController.js
@@ -1,10 +1,17 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+
 exports.register = async (req, res) => {
   try {
-    const { email, password, name, role } = req.body;
+    const { email, password, name } = req.body;
+
+    const isUserExist = await User.findOne({ where: { email } });
+    if (isUserExist) {
+      return res.status(400).json({ error: 'Ya hay un usuario registrado con ese email' });
+    }
+
     const hash = await bcrypt.hash(password, 10);
-    const user = await User.create({ email, password: hash, name, role });
+    const user = await User.create({ email, password: hash, name, role: 'student' });
     res.status(201).json({ message: 'Usuario creado', id: user.id });
   } catch (err) {
     res.status(400).json({ error: err.message });
