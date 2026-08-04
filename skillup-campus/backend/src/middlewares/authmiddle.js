c07a539b90184295b2d1524f9c4aeb1d695d4e2d
Fabricio <fabricionicolasr@gmail.com>
2026-08-04 01:56:23 -0300

    se configura authmiddle, se agrega a routes

--- skillup-campus/backend/src/middlewares/authmiddle.js
+++ skillup-campus/backend/src/middlewares/authmiddle.js
@@ -11,6 +11,6 @@ module.exports = (req, res, next) => {
         req.user = decoded;
         next();
     } catch (err) {
-        return res.status(401).json({ error: 'Token inválido' });
+        return res.status(401).json({ error: 'Acceso denegado' });
     }
 };
--- skillup-campus/backend/src/routes/auth.js
+++ skillup-campus/backend/src/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authmiddle');
 
+// Rutas de autenticación
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+// Ruta protegida para obtener el perfil del usuario
+router.get('/profile', authMiddleware);
 
 module.exports = router;
